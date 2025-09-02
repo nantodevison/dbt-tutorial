eb7643d52549eedaf1f55f33297a1ada9f814bf5
martin.schoreisz <martin.schoreisz@.cerema.fr>
2025-09-02 22:19:30 +0200

    feat/toutModeleParMacros

--- models/dept19/lin/lin_verif_gest_bdtopo_19.sql
+++ models/dept19/lin/lin_verif_gest_bdtopo_19.sql
@@ -4,26 +4,11 @@
 
 {% set annee_str = var('annee') | string %} 
 
-select
-    t.*,
-    row_number() over () as id
-from
-    (
-        select distinct
-            gestion gestionaire,
-            '_{{var('annee')}}' annee
-        from
-            {{ref('creer_vue_' ~ var('dept'))}}
-        order by
-            gestion
-    ) as t
+{{verifier_gestionnaire_bdtopo_lin()}}
 
-{#  on peut aussi faire appel à la macro dédiée. 
-    Je ne sais pas ce qui est le mieux, dc voici le code 
-    d'appell à la macro :
-    Dans sa forme basée sur les variables du dbt_project.yml :
-        {{verifier_gestionnaire_bdtopo_lin()}}
-        rappel : dbt run --vars '{"annee": 2024, "dept": "19"}'
+{#  Documentation d'utilisation :
+    appel avec spécification de variable: 
+        dbt run --vars '{"annee": 2024, "dept": "19"}'
     Dans sa forme basée sur les des variables passées manuellement :
         {{verifier_gestionnaire_bdtopo_lin(annee=2024, dept='19')}}
 #}
--- models/dept19/lin/lin_verif_nbpt_lgkm_19.sql
+++ models/dept19/lin/lin_verif_nbpt_lgkm_19.sql
@@ -4,17 +4,11 @@
 
 {% set annee_str = var('annee') | string %} 
 
-select count(distinct id_comptag) as nb_pt,
-       round(sum(long_km)::numeric) as sum_lg_km
-from {{ref('creer_vue_' ~ var('dept'))}}
-where id_comptag is not null
+{{ verifier_nbpt_lgkm_lin() }}
 
-{#  on peut aussi faire appel à la macro dédiée. 
-    Je ne sais pas ce qui est le mieux, dc voici le code 
-    d'appell à la macro :
-    Dans sa forme basée sur les variables du dbt_project.yml :
-    {{ verifier_nbpt_lgkm_lin() }}
-        rappel : dbt run --vars '{"annee": 2024, "dept": "19"}'
+{#  documentation d'utilisation :
+    appel avec spécification de variable : 
+        dbt run --vars '{"annee": 2024, "dept": "19"}'
     Dans sa forme basée sur les des variables passées manuellement :
-    {{ verifier_nbpt_lgkm_lin(annee=2024, dept='19') }}
+        {{ verifier_nbpt_lgkm_lin(annee=2024, dept='19') }}
 #}
