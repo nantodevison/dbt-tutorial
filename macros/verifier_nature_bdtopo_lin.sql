1ae75b2583e5b972d89473686a60c07093f5f95c
martin.schoreisz <martin.schoreisz@.cerema.fr>
2025-09-04 18:24:25 +0200

    feat/VariableInSchemaLin

--- macros/verifier_nature_bdtopo_lin.sql
+++ macros/verifier_nature_bdtopo_lin.sql
@@ -2,7 +2,7 @@
 
 SELECT coment_cpt,nature,round((sum(long_km))::numeric,2) as sum_lg_km, count(*) cnt 
 FROM {{ref('creer_vue_' ~ dept)}} 
-WHERE nature NOT IN {{var('nature_verif')}}
+WHERE nature != ALL(ARRAY{{var('nature_verif')}})
 GROUP BY coment_cpt, nature
 {% endmacro %}
 
