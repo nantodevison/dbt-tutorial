19dba5d4c8c5ff0c1d335528e3344f46fb75e0f3
Martin <martin.schoreisz@gmail.com>
2025-09-26 23:19:13 +0200

    Feat/EtireLin

---
+++ models/dept19/lin/lin_update_etire_lin_19.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    schema = 'affectation_pt_mano',
+    alias='traf' ~ var('annee') ~ '_bdt' ~ var('dept') ~'_ed' ~ (var('annee')|string)[-2:] ~ '_etirer_linearisation_l'
+    )
+}}
+
+{{ update_etirer_lin() }}
+
+{#  Documentation d'utilisation :
+    appel avec spécification de variable: 
+        dbt run --vars '{"dept": "19", "annee": "2024"}'
+    Dans sa forme basée sur les des variables passées manuellement :
+        {{update_etirer_lin(dept='19', annee=2024)}}
+#}
