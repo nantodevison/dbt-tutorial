b9f4f532dd8d921eecde075af6be159bad84c630
Martin <martin.schoreisz@gmail.com>
2026-03-15 22:39:12 +0100

    Fix/verifFonctOk

--- macros/verifier_valeurs_aberrantes_tmja.sql
+++ macros/verifier_valeurs_aberrantes_tmja.sql
@@ -6,10 +6,10 @@
     id_comptag_like=none,
     percentile_value=none) %}
 
-SELECT {% if not percentile_value %} * {% else %} PERCENTILE_DISC({{percentile_value}}) WITHIN GROUP(ORDER BY evol_n_n_1) as percentile_value {% endif %}
-FROM {{ cte_verif_valeurs_aberrantes_tmja__evol_ann_n_n_1(dept=dept) }}
-where abs(evol_n_n_1) > {{seuil_evol}} and abs(diff_n_n_1) > {{seuil_diff}} and annee_n = '{{ann_n_filter}}'
-{% if id_comptag_like %} and id_comptag like '{{id_comptag_like}}' {% endif %}
-{% if ann_n_1_filter %} and annee_n_1='{{ann_n_1_filter}}' {% endif %}
+SELECT {% if not percentile_value %} * {% else %} PERCENTILE_DISC({{percentile_value}}) WITHIN GROUP(ORDER BY t.evol_n_n_1) as percentile_value {% endif %}
+FROM ({{ cte_verif_valeurs_aberrantes_tmja__evol_ann_n_n_1(dept=dept) }}) t
+where abs(t.evol_n_n_1) > {{seuil_evol}} and abs(t.diff_n_n_1) > {{seuil_diff}} and t.annee_n = '{{ann_n_filter}}'
+{% if id_comptag_like %} and t.id_comptag like '{{id_comptag_like}}' {% endif %}
+{% if ann_n_1_filter %} and t.annee_n_1='{{ann_n_1_filter}}' {% endif %}
 
 {% endmacro %}
