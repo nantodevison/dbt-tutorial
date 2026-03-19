d64cb10c2ad594990aea6c8d5ec5a4260eda159b
Martin <martin.schoreisz@gmail.com>
2026-03-19 21:54:30 +0100

    feat/BaseDept19OK

---
+++ models/mdl_ad_troncons_new_millesime.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        schema='bascule_millesime'
+    )
+}}
+
+{{mcr_ajouter_troncon_nouveau_millesime()}}
+
+{#  documentation d'utilisation :
+        appel : dbt run --vqars '{"annee": 2024, "dept": "19"}'
+    Dans sa forme basée sur les des variables passées manuellement :
+    {{ mdl_ad_troncons_new_millesime(annee=2024, dept='19') }}
+#}
